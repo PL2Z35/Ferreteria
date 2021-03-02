c1d15c5dc7f5939fb65211b2c84239b570dc648b
Carlos_1007 <carlos.salamanca02@uptc.edu.co>
2021-03-02 08:26:48 -0500

    Login
    Se agrego un login que permite acceder como admistrador,

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express= require('express');
+const { render } = require('pug');
 const router= express.Router();
 
 router.get('/',(req,res)=>{
   res.render('index',{title:"Pagina Inicial"})
 });
+router.post('/logAdmin',(req,res)=>{
+    var user=req.body.usuario;
+    var pass=req.body.pass;
+    if(user=="admin" && pass=="admin"){
+        res.redirect('/administrador')
+    }else{
+        res.redirect('/');
+    }
 
+});
 
 module.exports = router;
