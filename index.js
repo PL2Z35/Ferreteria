e4a554af024bc37089d64456a400d2347f59e2ed
Carlos_1007 <carlos.salamanca02@uptc.edu.co>
2021-03-03 13:43:03 -0500

    recolocacion de pagina de administrador
    neuvamente se agregan algunos elementos en la parte de administrador

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const path = require('path');
+const indexRouter= require('./routes/index.js');
+const AdminRouter=require('./routes/admin.js');
 
 //App iniciada
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+//app.use('/',indexRouter);
 //Crear servidor
 app.get('/', function(req,res){
     res.render('index',{
         title: 'Ferreteria'
     })
 });
+app.use('/administrador',AdminRouter);
 
 //Carpeta publica
 app.use(express.static(path.join(__dirname, 'public')));
