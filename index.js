cb0bc3d39d6a75774591de090ee6ee26951cc83d
PL2Z35 <43555170+PL2Z35@users.noreply.github.com>
2021-03-05 21:51:27 -0500

    Arreglos interfaz
    Index

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+var body_parser=require('body-parser');
 const express = require('express');
 const path = require('path');
 const indexRouter= require('./routes/index.js');
 const AdminRouter=require('./routes/admin.js');
+const router = require('./routes/index.js');
 
 //App iniciada
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-//app.use('/',indexRouter);
+
 //Crear servidor
 app.get('/', function(req,res){
     res.render('index',{
         title: 'Ferreteria'
     })
+    
 });
+
+app.use(body_parser.urlencoded({extended:true}));
+app.use('/administrador',AdminRouter)
 app.use(' ',AdminRouter);
 app.use('/administrador/AnadirHerramientas',AdminRouter);
 
@@ -24,3 +30,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(4000, function(){
     console.log('Corriendo por el puerto 3000')
 })
+
+app.post('/log', (req,res)=>{
+    var user = req.body.user
+    var pass = req.body.pass
+	console.log(user+"-"+pass)
+	if(user=="admin"&&pass=='admin'){
+		res.redirect('/administrador')
+	}
+});
+
+app.post('/add', (req,res)=>{
+    var user = req.body.user
+    var pass = req.body.pass
+	console.log(user+"-"+pass)
+	if(user=="admin"&&pass=='admin'){
+		res.redirect('/administrador')
+	}
+});
+
+
+
