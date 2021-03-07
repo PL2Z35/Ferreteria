c88d618c2d051ac36da1e404164d74fe68b0466d
YuliethHuerfanoPerez <39287918+YuliethHuerfanoPerez@users.noreply.github.com>
2021-03-07 00:29:48 -0500

    Agregar datos a la colección JSON
    Métodos que escriben (agregan) en la colección JSON y eliminan

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@ const express= require('express');
 const router= express.Router();
 const app = express();
 const tools = [];
+var existe;
+var i;
+var estado;
 
 router.get('/',(req,res)=>{
   res.render('admin',{title:"Administrador"})
@@ -11,68 +14,103 @@ router.get('/AnadirHerramienta',(req,res)=>{
     res.render('newTools',{title:"Añadir Herramienta"})
 
 });
-//?
-router.get('/', (req, res)=>{
-  res.render('newTools');
-})
-
 
 router.get('/ActualizarHerramienta',(req,res)=>{
-    var Tools=[{"id":"22","name":"333"}];
-    res.render('updateTool',{title:"Actualizar herramienta",Tools})
+    res.render('updateTool',{title:"Actualizar herramienta",tools})
 });
+
 router.get('/VisualizarInventario',(req,res)=>{
     res.render('admin',{title:"PENDIENTE"})
 });
 router.get('/EliminarHerramienta',(req,res)=>{
-    var Tools=[{"id":"22","name":"333"}];
-    res.render('DeleteTool',{title:"Eliminar herramienta",Tools})
+    res.render('DeleteTool',{title:"Eliminar herramienta",tools})
 });
 
 //control de datos
 router.post('/newTools', (req, res) => {
-  //const {barCode, quantity, brand, price, description} = req.body;
-  const barcode= req.body.BarCode;
-  const quantity=req.body.Quantity;
-  const brand=req.body.Brand;
-  const price=req.body.Price;
-  const description=req.body.Description;
-  if(barcode!=null && quantity!=null && brand!=null && price!=null && description!=null ){
-    if(quantity>0 && price>0){
+  const {BarCode, Quantity, Brand, Price, Description} = req.body;
+  let newTool = {
+    BarCode,
+    Quantity,
+    Brand,
+    Price,
+    Description
+  }
+
+  for (i = 0; i < tools.length; i++){
+    barsCode = tools[i].BarCode;
+    if(barsCode == BarCode){
+      existe = i;
+      estado = true;
+    }
+  }
+
+  if(BarCode!=null && Quantity!=null && Brand!=null && Price!=null && Description!=null ){
+    if(Quantity>0 && Price>0 && estado!=true){
       res.render('admin',{title:"Administrador"})
+      tools.push(newTool);
     }else {
       res.render('newTools',{title:"Añadir Herramienta"})
     }
   }
 
+  console.log(tools);
 
-  /*if(!barsCode || !quantity || !brand || !price || !description){
-  res.status(400).send('Entradas incompletas');
-  return;
-  }
-  */
-  /*let newTool = {
-    barsCode,
-    quantity,
-    brand,
-    price,
-    description
-  }
-
-  tools.push(newTool);*/
 });
 
 router.post('/updatetool',(req,res)=>{
-  const barcode=req.body.Barcodes;
+    const {Barcodes, Quantity, Brand, Price, Description} = req.body;
+    let updateTool = {
+      Barcodes,
+      Quantity,
+      Brand,
+      Price,
+      Description
+    }
+  /*const barsCode;
+  const quantity;
+  const brand;
+  const price;
+  const description;
+  /*const barcode=req.body.Barcodes;
   const quantity=req.body.Quantity;
   const price=req.body.Price;
-  const description=req.body.Description;
+  const description=req.body.Description;*/
 
-  console.log("codigo"+ barcode +"canridad"+ quantity + "Precio" +price +"Descripcion"+description )
+
+  for (i = 0; i < tools.length; i++){
+    barsCode = tools[i].BarCodes;
+    quantity = tools[i].Quantity;
+    price = tools[i].Price;
+    description = tools[i].Description;
+    if(Quantity!=null && Price!=null && Description!=null ){
+      if(Quantity>0 && Price>0){
+        res.render('admin',{title:"Administrador"})
+        tools.update(updateTool);
+      }else {
+        res.render('newTools',{title:"Añadir Herramienta"})
+      }
+    }
+  }
+
 });
+
+
 router.post('/deletetool',(req,res)=>{
-  const barcode=req.body.Barcodes;
-  console.log("LLegando ="+barcode)
+  //const {Barcodes, Quantity, Brand, Price, Description} = req.body;
+  var dato;
+  //const barcode=req.body.Barcodes;
+  const barcode=req.body.BarCode;
+  for (i = 0; i < tools.length; i++){
+    barsCode = tools[i].BarCodes;
+    if(barsCode == barcode){
+      dato = tools[i].BarCodes;
+      existe = i;
+      estado = true;
+    }
+  }
+  tools.pop(dato);
+  console.log(tools);
 });
 
 
